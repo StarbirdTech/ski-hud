187fe18da079dd530a6403aeea0d09c75d2ab97a
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2023-02-08 13:27:52 -0500

    functional prototype

---
+++ shadow-removal.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+img = cv2.imread("test1.jpg")
+
+def shadow_remove(img):
+    rgb_planes = cv2.split(img)
+    result_norm_planes = []
+    for plane in rgb_planes:
+        dilated_img = cv2.dilate(plane, np.ones((7, 7), np.uint8))
+        bg_img = cv2.medianBlur(dilated_img, 21)
+        diff_img = 255 - cv2.absdiff(plane, bg_img)
+        norm_img = cv2.normalize(diff_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+        result_norm_planes.append(norm_img)
+    shadowremov = cv2.merge(result_norm_planes)
+    return shadowremov  # Shadow removal
+shad = shadow_remove(img)
+cv2.imwrite('after_shadow_remove1.jpg', shad)
