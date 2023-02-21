2a2f5711d7aa7022b1cd4ff7a4fae76229f1bc87
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2023-02-21 14:12:03 -0500

    send resized data over serial to nano

--- contourDetect.py
+++ contourDetect.py
@@ -1,7 +1,8 @@
 import cv2
 import serial
+import json
 
-ser = serial.Serial('COM4')
+ser = serial.Serial('COM12', 1000000)
 print(ser.name)
 
 vid = cv2.VideoCapture(0)
@@ -9,12 +10,29 @@ if not vid.isOpened():
     print("Cannot open camera")
     exit()
 
+frameW = int(vid.get(3))
+frameH = int(vid.get(4))
+frameRatio = frameW / frameH
+
+screenW = 128
+screenH = 64
+screenRatio = screenW / screenH
+
 objects = []
 
 while (True):
     ret, frame = vid.read()
 
     if ret == True:
+        if frameRatio == screenRatio:
+            frame = frame[0:screenW, 0:screenH]
+        elif frameRatio > screenRatio:
+            frame = frame[0:frameH, int((frameW - (frameH * screenRatio)) / 2):int((frameW + (frameH * screenRatio)) / 2)]
+        else:
+            frame = frame[int((frameH - (frameW / screenRatio)) / 2):int((frameH + (frameW / screenRatio)) / 2), 0:frameW]
+
+        # Resize the image to the desired size
+        frame = cv2.resize(frame, (screenW, screenH), interpolation=cv2.INTER_AREA)
 
         # Convert the image to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -53,8 +71,8 @@ while (True):
             objects.append((center_x, center_y))
           
             # Draw the contour and center position on the image
-            cv2.drawContours(frame, [cnt], 0, (0, 255, 0), 2)
-            cv2.circle(frame, (center_x, center_y), 2, (0, 0, 255), -1)
+            #cv2.drawContours(frame, [cnt], 0, (0, 255, 0), 2)
+            cv2.rectangle(frame, (center_x-10, center_y-10), (center_x+10, center_y+10), (0, 0, 255))
 
         # Display the image
         cv2.imshow('Image', frame)
@@ -62,7 +80,7 @@ while (True):
         #send object coordinates to arduino and clear objects list
         if len(objects) > 0:
             print(objects)
-            ser.write(str(objects).encode())
+            ser.write(json.dumps(objects).encode())
             objects.clear()
 
         # Press S on keyboard to stop the process
